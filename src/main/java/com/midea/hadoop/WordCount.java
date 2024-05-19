9b2cfb057d0f0f674e439c41d03c4d3c0ed253c3
“bruce256” <326375225@qq.com>
2024-05-19 18:54:51 +0800

    自定义分区

--- src/main/java/com/midea/hadoop/WordCount.java
+++ src/main/java/com/midea/hadoop/WordCount.java
@@ -26,65 +26,83 @@ import java.util.StringTokenizer;
  * @date 2019-09-01
  **/
 public class WordCount {
-
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
-        private final static IntWritable one  = new IntWritable(1);
-        private              Text        word = new Text();
-
-        @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
-        }
-    }
-
-
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        private IntWritable result = new IntWritable();
-
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
-            throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            result.set(sum);
-            context.write(key, result);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        long          start = System.currentTimeMillis();
-
-        // 1. 获取配置文件对象，获取job对象实例
-        Configuration conf  = new Configuration();
-        Job           job   = Job.getInstance(conf, "word count");
-
-        // 2. 指定程序jar的本地路径
-        job.setJarByClass(WordCount.class);
-
-        //  3. 指定Mapper/Reducer类
-        job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(IntSumReducer.class);
-
-        // 5. 指定最终输出的kv数据类型
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        // 6. 指定job处理的原始数据路径
-        FileInputFormat.addInputPath(job, new Path("/Users/LvSheng/code/github/Test/data.txt"));
-        //  7. 指定job输出结果路径
-        FileOutputFormat.setOutputPath(job, new Path("/Users/LvSheng/code/github/Test/data_out"));
-
-        //  8. 提交作业
-        job.waitForCompletion(true);
-
-        System.out.println("time cost ： " + (System.currentTimeMillis() - start) / 1000 + " s");
-    }
+	
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+		
+		private final static IntWritable one  = new IntWritable(1);
+		private              Text        word = new Text();
+		
+		@Override
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+	
+	
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		private IntWritable result = new IntWritable();
+		
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		try {
+			long start = System.currentTimeMillis();
+			
+			// 1. 获取配置文件对象，获取job对象实例
+			Configuration conf = new Configuration();
+			Job           job  = Job.getInstance(conf, "word count");
+			
+			// 2. 指定程序jar的本地路径
+			job.setJarByClass(WordCount.class);
+			
+			//  3. 指定Mapper/Reducer类
+			job.setMapperClass(TokenizerMapper.class);
+			job.setReducerClass(IntSumReducer.class);
+			
+			job.setNumReduceTasks(27);
+			job.setPartitionerClass(MyPartitioner.class);
+			
+			// 5. 指定最终输出的kv数据类型
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(IntWritable.class);
+			
+			// 6. 指定job处理的原始数据路径
+			FileInputFormat.addInputPath(job, new Path("/Users/LvSheng/Documents/aclImdb_v1_train_datasets/pos"));
+//		FileInputFormat.addInputPath(job, new Path("/Users/LvSheng/code/github/hadoop-test/data.txt"));
+			//  7. 指定job输出结果路径
+			FileOutputFormat.setOutputPath(job, new Path("/Users/LvSheng/code/github/hadoop-test/data_out"));
+			
+			//  8. 提交作业
+			job.waitForCompletion(true);
+			
+			System.out.println("time cost ： " + (System.currentTimeMillis() - start) / 1000 + " s");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
